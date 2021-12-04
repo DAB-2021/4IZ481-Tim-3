cca9ebad7f1684d97bf0dd1918febb8481fc8899
aheldes <59527963+aheldes@users.noreply.github.com>
2021-12-04 23:14:30 +0100

    Almost there

--- actions/__init__.py
+++
@@ -1,5 +0,0 @@
-# from sanic import Sanic
-# from my_blueprint import bp
-
-# app = Sanic(__name__)
-# app.blueprint(bp)
--- actions/my_blueprint.py
+++
@@ -1,8 +0,0 @@
-from sanic.response import json
-from sanic import Blueprint
-
-bp = Blueprint("my_blueprint")
-
-@bp.route("/test")
-async def bp_root(request):
-    return json({"my": "blueprint"})
